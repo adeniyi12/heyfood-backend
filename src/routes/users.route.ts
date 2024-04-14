86f7e27374966e55608196041a841c761fb93015
Timilehin Adeleye <timi.adeleye@babbangona.com>
2024-04-14 09:23:05 +0100

    heyfood project

---
+++ src/routes/users.route.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { UserController } from '@controllers/users.controller';
+import { CreateUserDto } from '@dtos/users.dto';
+import { Routes } from '@interfaces/routes.interface';
+
+export class UserRoute implements Routes {
+  public path = '/users';
+  public router = Router();
+  public user = new UserController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.user.getUsers);
+    this.router.get(`${this.path}/:id`, this.user.getUserById);
+    this.router.delete(`${this.path}/:id`, this.user.deleteUser);
+  }
+}
